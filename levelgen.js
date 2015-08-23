da38c4d8912561f944dbf345f508ef5f4fedf725
Arthur Langereis <puurtuur@me.com>
2015-08-23 16:31:10 +0200

    levelgen now sets up fixed camera points and 2d collision grid as well

--- levelgen.js
+++ levelgen.js
@@ -1,11 +1,16 @@
+// Part of Pinky's Nightmare
+// (c) 2015 by Arthur Langereis - @zenmumbler
+
+var LEVEL_SCALE = 4.0;
+
+
 function buildMapFromImageData(pix) {
 	var inuse = 0, pixw = pix.width, pixh = pix.height;
-	var data = pix.data, offset = 0;
+	var data = pix.data, offset = 0, gridOffset = 0;
 	
-	var SCALE = 4.0;
-	var HEIGHT = 20.0;       // will appear inf high 
+	var HEIGHT = 30.0;       // will appear inf high 
 	
-	var vertexes = [], normals = [], colors = [];
+	var vertexes = [], normals = [], colors = [], cameras = [], grid = [];
 	function vtx(x, y, z) { vertexes.push(x, y, z); }
 	function nrm6(n) { for(var n=0; n<6; ++n) normals.push(n[0], n[1], n[2]); }
 	
@@ -17,61 +22,70 @@ function buildMapFromImageData(pix) {
 	for (var z=0; z < pixh; ++z) {
 		for (var x=0; x < pixw; ++x) {
 			if (data[offset] == 0) {
-				++inuse;
-				
-				var xa = x * SCALE,
-					xb = (x+1) * SCALE,
-					za = z * SCALE;
-					zb = (z+1) * SCALE,
+				var xa = x * LEVEL_SCALE,
+					xb = (x+1) * LEVEL_SCALE,
+					za = z * LEVEL_SCALE,
+					zb = (z+1) * LEVEL_SCALE,
 					h = HEIGHT;
 				
-				// ccw
-				// wall top
-				vtx(xb, h, za);
-				vtx(xb, 0, za);
-				vtx(xa, 0, za);
-
-				vtx(xa, 0, za);
-				vtx(xa, h, za);
-				vtx(xb, h, za);
+				grid[gridOffset] = false;
 				
-				nrm6(north);
-
-				// wall left
-				vtx(xa, h, za);
-				vtx(xa, 0, za);
-				vtx(xa, 0, zb);
-
-				vtx(xa, 0, zb);
-				vtx(xa, h, zb);
-				vtx(xa, h, za);
-
-				nrm6(west);
+				if (data[offset+1] > 200) {
+					cameras.push(vec2.fromValues(x + .5, z + .5));
+				}
+				else {
+					++inuse;
+					grid[gridOffset] = true;
 				
-				// wall bottom
-				vtx(xa, h, zb);
-				vtx(xa, 0, zb);
-				vtx(xb, 0, zb);
+					// ccw
+					// wall top
+					vtx(xb, h, za);
+					vtx(xb, 0, za);
+					vtx(xa, 0, za);
 
-				vtx(xb, 0, zb);
-				vtx(xb, h, zb);
-				vtx(xa, h, zb);
-
-				nrm6(south);
-
-				// wall right
-				vtx(xb, h, zb);
-				vtx(xb, 0, zb);
-				vtx(xb, 0, za);
+					vtx(xa, 0, za);
+					vtx(xa, h, za);
+					vtx(xb, h, za);
 				
-				vtx(xb, 0, za);
-				vtx(xb, h, za);
-				vtx(xb, h, zb);
+					nrm6(north);
 
-				nrm6(east);
+					// wall left
+					vtx(xa, h, za);
+					vtx(xa, 0, za);
+					vtx(xa, 0, zb);
+
+					vtx(xa, 0, zb);
+					vtx(xa, h, zb);
+					vtx(xa, h, za);
+
+					nrm6(west);
+				
+					// wall bottom
+					vtx(xa, h, zb);
+					vtx(xa, 0, zb);
+					vtx(xb, 0, zb);
+
+					vtx(xb, 0, zb);
+					vtx(xb, h, zb);
+					vtx(xa, h, zb);
+
+					nrm6(south);
+
+					// wall right
+					vtx(xb, h, zb);
+					vtx(xb, 0, zb);
+					vtx(xb, 0, za);
+				
+					vtx(xb, 0, za);
+					vtx(xb, h, za);
+					vtx(xb, h, zb);
+
+					nrm6(east);
+				}
 			}
 
 			offset += 4;
+			gridOffset++;
 		}
 	}
 	
@@ -81,9 +95,15 @@ function buildMapFromImageData(pix) {
 	}
 
 	console.info("inuse", inuse);
-	console.info("vtx", vertexes.length);
+	console.info("vtx", vertexes.length, "cam", cameras.length);
 
-	return new TriMesh(vertexes, normals, colors);
+	return {
+		cameras: cameras,
+		grid: grid,
+		gridW: pixw,
+		gridH: pixh,
+		mesh: new TriMesh(vertexes, normals, colors)
+	};
 }
 
 
@@ -102,7 +122,7 @@ function genMapMesh(then) {
 		var map = buildMapFromImageData(pix);
 		var t1 = performance.now();
 		
-		console.info("mapGen took", t1-t0, "ms");
+		console.info("mapGen took", (t1-t0).toFixed(2), "ms");
 		
 		then(map);
 	};
